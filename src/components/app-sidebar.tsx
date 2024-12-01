b85d9ce47a7a560838a7597937c26cd927a5a0fa
zahidalidev <m.zahidalidev@gmail.com>
2024-12-01 21:27:49 +0500

    Added wave animation

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -22,10 +22,11 @@ import Image from "next/image"
 
 import CodyLogo from '@/assets/icons/codyIconWithText.svg'
 import { Button } from "./ui/button"
+import { motion } from "framer-motion"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { items, activeItem, activeSubItem, handleItemSelect } = useTourStore()
-
+  const [isMoving, setIsMoving] = React.useState(false)
 
   return (
     <Sidebar variant="inset" {...props}>
@@ -38,7 +39,59 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
-      <SidebarContent>
+      <SidebarContent className="relative">
+        {activeSubItem && (
+          <motion.div
+            className="absolute left-[12px] z-10"
+            initial={false}
+            layoutId="submenu-indicator"
+            animate={{
+              top: `${(() => {
+                const activeItemIndex = items.findIndex(item => item.id === activeItem.id)
+                const activeSubItemIndex = activeItem.subItems.findIndex(subItem => subItem.id === activeSubItem.id)
+
+                const subOffset = activeItemIndex == 0 ? 31 : 30.5
+                const previousItemsOffset = items
+                  .slice(0, activeItemIndex)
+                  .reduce((acc, item) => acc + (item.subItems?.length || 0) * subOffset, 0)
+                const mainItemsOffset = activeItemIndex * 40
+                const currentSubItemOffset = activeSubItemIndex * subOffset
+
+                const baseOffset = 45
+                return baseOffset + mainItemsOffset + previousItemsOffset + currentSubItemOffset
+              })()}px`
+            }}
+            onAnimationStart={() => setIsMoving(true)}
+            onAnimationComplete={() => setIsMoving(false)}
+            transition={{
+              type: "spring",
+              stiffness: 500,
+              damping: 35,
+              mass: 0.5
+            }}
+          >
+            <div className="w-2 h-2 bg-white rounded-full" />
+            {!isMoving && <motion.div
+              className="absolute w-2 h-2 rounded-full border border-white opacity-60"
+              animate={{
+                opacity: [0.6, 0],
+                scale: [1, 3]
+              }}
+              transition={{
+                duration: 2.5,
+                times: [0, 1],
+                repeat: Infinity,
+                // delay: 0.8,
+                ease: "linear",
+                repeatDelay: 0
+              }}
+              style={{
+                top: 0,
+                left: 0
+              }}
+            />}
+          </motion.div>
+        )}
         <SidebarMenu>
           {items.map((item) => (
             <Collapsible key={item.id} asChild defaultOpen={item.isActive}>
@@ -58,12 +111,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </SidebarMenuButton>
                 {item.subItems?.length && (
                   <>
-                    {/* <CollapsibleTrigger asChild>
-                      <SidebarMenuAction className="data-[state=open]:rotate-90">
-                        <ChevronRight />
-                        <span className="sr-only">Toggle</span>
-                      </SidebarMenuAction>
-                    </CollapsibleTrigger> */}
                     <CollapsibleContent>
                       <SidebarMenuSub>
                         {item.subItems.map((subItem) => (
@@ -89,7 +136,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           ))}
         </SidebarMenu>
       </SidebarContent>
-
       <SidebarFooter>
         <SidebarMenu>
           <SidebarMenuItem>
